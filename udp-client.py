4cd94c236ade365b32ffd83f96668db8f39d5803
wynemo <ggdabin@gmail.com>
2014-03-21 16:20:26 +0800

    pre allocate connection

--- udp-client.py
+++ udp-client.py
@@ -6,38 +6,68 @@ import select
 import SocketServer
 import logging
 import ssl
+import threading
+import struct
+import random
+
+ssl_sockets = []
+
+for i in xrange(0, 6):
+    remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssl_socket = ssl.wrap_socket(remote, ssl_version=ssl.PROTOCOL_TLSv1)
+    l = ('198.58.115.107', 63235)
+    ssl_socket.connect(l)
+    ssl_sockets.append((ssl_socket, threading.RLock()))
+print 21, ',,,,,'
+d = {}
+
+def foo():
+    global ssl_socket
+    global d
+    while True:
+        r, w, e = select.select([ssl_socket], [], [])
+        if ssl_socket in r:
+            data = ssl_socket.recv(64*1024)
+            print 27, len(data)
+            port = struct.unpack('<H', data[-2:])[0]
+            print 29, port
+            sock = d[port]
+            d.pop(port)
+            sock.sendto(data[:-2], ('127.0.0.1', port))
 
 class Encoder(SocketServer.DatagramRequestHandler):
     
     def handle(self):
+        global ssl_sockets
+        #global d
+        print 43, self.client_address
+        random.seed()
+        i = random.randint(0, 5)
+        ssl_socket, lock = ssl_sockets[i]
+        
         data, sock = self.request
-        # socket.sendto(data, self.client_address)
-        remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ssl_socket = ssl.wrap_socket(remote, ssl_version=ssl.PROTOCOL_TLSv1)
-        l = ('198.58.115.107', 63235)
-        ssl_socket.connect(l)
-        ssl_socket.send(data)
-
-        r, w, e = select.select([ssl_socket], [], [])
-        try:
-            if ssl_socket in r:
-                data = ssl_socket.recv(64*1024)
-                if data:
-                    sock.sendto(data, self.client_address)
-        except socket.sslerror as e:
-            if e.args[0] != socket.SSL_ERROR_EOF:
-                raise
-
-        ssl_socket.close()
-                        
-
+        
+        port = self.client_address[1]
+        #d[port] = sock
+        
+        with lock:
+            ssl_socket.send(data + struct.pack('<H', port))
+            data = ssl_socket.recv(64*1024)
+            #port = struct.unpack('<H', data[-2:])[0]
+        print 57, self.client_address, len(data)
+        sock.sendto(data[:-2], (self.client_address[0], port))
+            
 def main():
     level = logging.INFO
     logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p',
         level=level)
-    server = SocketServer.ThreadingUDPServer(('127.0.0.1', 53), Encoder)
+    #t = threading.Thread(target=foo)
+    #t.daemon = True
+    #t.start()
+    server = SocketServer.ThreadingUDPServer(('0.0.0.0', 53), Encoder)
     server.serve_forever()
+    ssl_socket.close()
     
 if __name__ == '__main__':
     main()
--- udp-server.py
+++ udp-server.py
@@ -21,24 +21,14 @@ class SSlSocketServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 
 class Decoder(SocketServer.StreamRequestHandler):
     def handle_tcp(self, sock, remote):
-        fdset = [sock, remote]
-        while True:
-            r, w, e = select.select(fdset, [], [])
-            try:
-                if sock in r:
-                    data = sock.recv(64*1024)
-                    if not data:
-                        break
-                    else:
-                        remote.sendto(data, (UDP_IP, UDP_PORT))
-                if remote in r:
-                    data, addr = remote.recvfrom(64*1024)
-                    sock.send(data)
-            except socket.sslerror, x:
-                if x.args[0] == socket.SSL_ERROR_EOF:
-                    break
-                else:
-                    raise
+        while 1:
+            data = sock.recv(64*1024)
+            if data:
+                remote.sendto(data[:-2], (UDP_IP, UDP_PORT))
+                new_data, addr = remote.recvfrom(64*1024)
+                sock.send(new_data + data[-2:])
+            else:
+                break
 
     def handle(self):
         remote = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
