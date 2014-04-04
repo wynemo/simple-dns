bd352fcdd0699f4e637f1e6e9777fb62930b94c4
wynemo <ggdabin@gmail.com>
2014-04-04 09:56:44 +0800

    add reconnect

--- client.py
+++ client.py
@@ -7,28 +7,44 @@ import ssl
 import threading
 import random
 import json
+import errno
 
 ssl_sockets = []
 CONNECTION_NUM = 5
 CONFIG_FILE = 'config.json'
-
+server_ip = None
+server_port = None
 
 class Encoder(SocketServer.DatagramRequestHandler):
     
     def handle(self):
         global ssl_sockets
+        global server_ip
+        global server_port
         random.seed()
         i = random.randint(0, CONNECTION_NUM - 1)
         ssl_socket, lock = ssl_sockets[i]
         data, sock = self.request
         with lock:
-            ssl_socket.send(data)
-            data = ssl_socket.recv(64*1024)
+            try:
+                ssl_socket.sendall(data)
+                data = ssl_socket.recv(64*1024)
+            except socket.error as error:
+                if error.errno == errno.WSAECONNRESET:
+                    remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    ssl_socket = ssl.wrap_socket(remote, ssl_version=ssl.PROTOCOL_TLSv1)
+                    l = (server_ip, server_port)
+                    ssl_socket.connect(l)
+                    ssl_sockets[i] = ssl_socket, threading.RLock(),
+                else:
+                    raise
         sock.sendto(data, self.client_address)
 
 
 def main():
     global ssl_sockets
+    global server_ip
+    global server_port
 
     with open(CONFIG_FILE, 'rb') as f:
         o = json.load(f)
